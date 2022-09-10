5cd7efd0b57a6b4fbb6d9a599a5ce5f6c9f30b53
Marcelo Sant'Anna <marcelojunin2010@hotmail.com>
2022-09-10 18:44:23 -0300

    Bridge implementation to database types

--- src/main/java/br/com/cod3r/bridge/mvc/Client.java
+++ src/main/java/br/com/cod3r/bridge/mvc/Client.java
@@ -1,13 +1,30 @@
 package br.com.cod3r.bridge.mvc;
 
+import br.com.cod3r.bridge.mvc.dao.UserMongoDao;
+import br.com.cod3r.bridge.mvc.dao.UserMysqlDao;
+import br.com.cod3r.bridge.mvc.dao.UserOracleDao;
+import br.com.cod3r.bridge.mvc.dao.UserPostgresDao;
 import br.com.cod3r.bridge.mvc.model.User;
+import br.com.cod3r.bridge.mvc.services.UserEJB;
+import br.com.cod3r.bridge.mvc.services.UserRest;
+import br.com.cod3r.bridge.mvc.services.UserSoap;
 
 public class Client {
 
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		User user = new User("user", "user@email.com", "passwd");
-		
-		
+
+		final UserRest userRest = new UserRest(new UserOracleDao());
+		userRest.save(user);
+
+		final UserEJB userEJB = new UserEJB(new UserMongoDao());
+		userEJB.save(user);
+
+		final UserSoap userSoap = new UserSoap(new UserPostgresDao());
+		userSoap.save(user);
+
+		final UserRest userRestMysql = new UserRest(new UserMysqlDao());
+		userRestMysql.save(user);
 	}
 }
