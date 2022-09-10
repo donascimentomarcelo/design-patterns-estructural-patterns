970826b35ae7681bd49bd2c12d72feefd1c75ff4
Marcelo Sant'Anna <marcelojunin2010@hotmail.com>
2022-09-10 17:30:22 -0300

    Bridge implementation to converter

--- src/main/java/br/com/cod3r/bridge/converter/Client.java
+++ src/main/java/br/com/cod3r/bridge/converter/Client.java
@@ -1,8 +1,26 @@
 package br.com.cod3r.bridge.converter;
 
+import br.com.cod3r.bridge.converter.converters.CSVConverter;
+import br.com.cod3r.bridge.converter.converters.Converter;
+import br.com.cod3r.bridge.converter.converters.JsonConverter;
+import br.com.cod3r.bridge.converter.employees.ITGuy;
+import br.com.cod3r.bridge.converter.employees.ProjectManager;
+
 public class Client {
 
 	public static void main(String[] args) {
-		
+		final Converter csvConverter = new CSVConverter();
+		final Converter jsonConverter = new JsonConverter();
+
+		final ITGuy itGuy = new ITGuy("Joe", 20, 4000d);
+		final ProjectManager projectManager = new ProjectManager("Bob", 30, 6000d);
+
+		System.out.println(csvConverter.getEmployeeFormated(itGuy));
+		System.out.println(jsonConverter.getEmployeeFormated(itGuy));
+
+		System.out.println("-----###-----");
+
+		System.out.println(csvConverter.getEmployeeFormated(projectManager));
+		System.out.println(jsonConverter.getEmployeeFormated(projectManager));
 	}
 }
